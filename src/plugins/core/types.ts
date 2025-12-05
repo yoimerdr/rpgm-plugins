ecb9595fa3be10330d41595a4c2dfeb15b76d4be
Davila Yoimer <89022117+yoimerdr@users.noreply.github.com>
2025-12-04 19:47:02 -0500

    feat: add type definitions for Core plugin extensions and polyfills

---
+++ src/plugins/core/types.ts
@@ -0,0 +1,37 @@
+import {YDPCore} from "./index.js";
+import {NumberExtensions} from "./modules/extensions/number";
+import {StringExtensions} from "./modules/extensions/string";
+import {ArrayExtensions} from "./modules/extensions/array";
+import {StringPolyfill} from "./modules/polyfills/string";
+import {ArrayConstructorPolyfill} from "./modules/polyfills/array";
+import {ObjectConstructorPolyfill} from "./modules/polyfills/object";
+
+declare const YDP_Core: YDPCore
+
+// extensions
+declare global {
+  interface ArrayConstructor extends ArrayConstructorPolyfill {
+
+  }
+
+  interface ObjectConstructor extends ObjectConstructorPolyfill {
+
+  }
+
+  interface Array<T> extends ArrayExtensions<T> {
+
+  }
+
+  interface Number extends NumberExtensions {
+
+  }
+
+  interface String extends StringExtensions, StringPolyfill {
+  }
+}
+
+
+/**
+ * The global instance of the Core plugin.
+ */
+export default YDP_Core
