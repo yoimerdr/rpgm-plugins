7d42eea8597bba5008fa0d3844354cddf79cd050
Davila Yoimer <89022117+yoimerdr@users.noreply.github.com>
2025-12-05 19:26:26 -0500

    fix: move setWindowPolyfill function to properties module and update its implementation

--- src/plugins/core/modules/properties.ts
+++ src/plugins/core/modules/properties.ts
@@ -23,7 +23,31 @@ import {keys, prototype} from "@jstls/core/shortcuts/object";
 import {name} from "@jstls/core/functions";
 import {uid} from "@jstls/core/polyfills/symbol";
 import {simple} from "@jstls/core/definer/getters/builders";
-import {setWindowPolyfill} from "@core-plugin/modules/polyfills";
+import {isFunction} from "@jstls/core/objects/types";
+import {get2} from "@jstls/core/objects/handlers/getset";
+import {win} from "@jstls/components/shared/constants";
+
+/**
+ * The polyfill inclusion mode.
+ *
+ * - `auto`: Automatically determine if the polyfill is needed.
+ * - `include`: Force inclusion of the polyfill.
+ * - `exclude`: Force exclusion of the polyfill.
+ */
+export type PolyfillMode = "auto" | "include" | "exclude";
+
+/**
+ * Sets a polyfill on `window` object based on the provided mode and availability.
+ *
+ * @param mode The polyfill mode (`auto`, `include`, `exclude`).
+ * @param name The name of the property to polyfill on the global object.
+ * @param polyfill The polyfill implementation.
+ */
+export function setWindowPolyfill(mode: PolyfillMode, name: string, polyfill: any) {
+  if (mode === "include" || (mode === "auto" && !isDefined(get2(win, name)))) {
+    define(win, name, descriptor(polyfill, true, true, !isFunction(polyfill)))
+  }
+}
 
 /**
  * Defines a property on an object.
