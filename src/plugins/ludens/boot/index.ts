af44dba876ce05c1c2e204d8988c581754ef7374
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-01-27 16:48:45 -0500

    feat: enhance applyBoot to handle CSS font loading compatibility in Graphics

--- src/plugins/ludens/boot/index.ts
+++ src/plugins/ludens/boot/index.ts
@@ -2,8 +2,24 @@ import {method} from "@jstls/core/extender";
 import {prototype} from "@jstls/core/shortcuts/object";
 import {events} from "@ludens-plugin/modules/events";
 import {KeyableObject} from "@jstls/types/core/objects";
+import {isFunction} from "@jstls/core/objects/types";
+import {doc} from "@jstls/components/shared/constants";
+import {apply} from "@jstls/core/functions/apply";
+import {set2} from "@jstls/core/objects/handlers/getset";
 
 export function applyBoot() {
+  method(
+    Graphics as KeyableObject,
+    "_setupCssFontLoading",
+    {
+      replace(source) {
+        if (isFunction(doc.fonts.ready.then))
+          apply(source, this)
+        else set2(Graphics, "_cssFontLoading", undefined);
+      }
+    }
+  )
+
   let loaded = false;
   method(
     prototype(Scene_Title),
