5a793ee14dbca4b630b10c332df3a76af825e648
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-01-27 16:50:39 -0500

    fix: prevent errors if LudensBridge is undefined in applyBoot

--- src/plugins/ludens/boot/index.ts
+++ src/plugins/ludens/boot/index.ts
@@ -5,7 +5,7 @@ import {KeyableObject} from "@jstls/types/core/objects";
 import {isFunction} from "@jstls/core/objects/types";
 import {doc} from "@jstls/components/shared/constants";
 import {apply} from "@jstls/core/functions/apply";
-import {set2} from "@jstls/core/objects/handlers/getset";
+import {get2, set2} from "@jstls/core/objects/handlers/getset";
 
 export function applyBoot() {
   method(
@@ -31,10 +31,16 @@ export function applyBoot() {
 
         loaded = true;
         events.emit("onload");
-        (window as KeyableObject).LudensBridge.callNative("LudensLoader", JSON.stringify({
-          isEnabled: true,
-          isLoading: false
-        }));
+        const bridge = get2(window, "LudensBridge");
+        if (bridge && bridge.callNative) {
+          bridge.callNative(
+            "LudensLoader",
+            JSON.stringify({
+              isEnabled: true,
+              isLoading: false
+            })
+          );
+        }
       }
     }
   )
