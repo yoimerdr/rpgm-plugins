2bac45461e3886a6ece9b30c5695b4836ca0f9fa
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-04-23 20:22:53 -0500

    fix: add missing export

--- src/plugins/crossassets/index.ts
+++ src/plugins/crossassets/index.ts
@@ -1,5 +1,6 @@
 import {KeyableObject} from "@jstls/types/core/objects";
 import {applyFullPlugin} from "@crossassets-plugin/boot";
+import {handler, PluginHandler} from "./handler";
 
 declare const exports: KeyableObject;
 
@@ -15,7 +16,12 @@ applyFullPlugin();
  * Provides access to plugin functionality and configuration.
  */
 export interface YDPCrossAssets {
+  /**
+   * The plugin handler instance that provides methods to configure and retrieve asset sources.
+   * */
+  handler: PluginHandler;
 }
 
 export {
+  handler
 }
